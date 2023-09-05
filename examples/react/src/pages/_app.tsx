b1cb61948201872e04f004ab4d05750285adbfa0
Glitch <glitch.txs@gmail.com>
2023-09-05 17:43:04 -0300

    add coinbase

--- examples/react/src/pages/_app.tsx
+++ examples/react/src/pages/_app.tsx
@@ -2,15 +2,28 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { W3, initW3, Injected } from '@w3vm/react'
 import walletconnect from 'public/walletconnect.svg'
+import coinbase from 'public/coinbase.svg'
 import wallet from 'public/wallet.png'
 import { WalletConnect } from '@w3vm/walletconnect'
+import { Coinbase } from '@w3vm/coinbase'
 
 const projectId = process.env.NEXT_PUBLIC_PROJECT_ID as string
 
 const w3props = initW3({
   connectors: [
     new Injected({ icon: wallet }), 
-    new WalletConnect({ projectId, icon: walletconnect, showQrModal: true, chains:[1] })
+    new WalletConnect({ 
+      projectId, 
+      icon: walletconnect, 
+      showQrModal: true, 
+      chains:[1] 
+    }),
+    new Coinbase({ 
+      appName:'Test', 
+      icon: coinbase, 
+      defaultChainId: 1, 
+      defaultJsonRpcUrl:'https://mainnet.infura.io/v3/'
+    })
   ],
   SSR: true,
 })
