be70132a988753bd561e4ee3d9dc119b1df818e1
Glitch <glitch.txs@gmail.com>
2023-09-30 03:25:26 -0300

    finish example

--- examples/web3modal/next/src/pages/_app.tsx
+++ examples/web3modal/next/src/pages/_app.tsx
@@ -2,12 +2,17 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { createWeb3Modal } from '@w3vm/web3modal'
 import { createEthers } from '@w3vm/web3modal-ethers-5'
-import { Injected } from '@w3vm/react'
+import { W3 } from '@w3vm/react'
 
 const projectId = 'cdbd18f9f96172be74c3e351ce99b908'
 
-createWeb3Modal({ projectId, chains:[1, 137], plugin: createEthers(), connectors:[new Injected()] })
+const w3props = createWeb3Modal({ projectId, chains:[1, 137], plugin: createEthers() })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <W3 {...w3props}/>
+      <Component {...pageProps} />
+    </>
+  )
 }
