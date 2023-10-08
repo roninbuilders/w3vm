8c94f9998fe324a7f9202b7f8c65ca258a2bd400
Glitch <glitch.txs@gmail.com>
2023-10-08 13:15:02 -0300

    docs

---
+++ docs/theme.config.jsx
@@ -0,0 +1,14 @@
+export default {
+  logo: <span>W3Vm</span>,
+  project: {
+    link: 'https://github.com/w3vm/w3vm-docs'
+  },
+  head: (
+    <>
+      <title>W3Vm</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="W3Vm" />
+      <meta property="og:description" content="Wallets <> Dapps connectors library" />
+    </>
+  )
+}
